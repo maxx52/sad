a1b4bd29a8762b4eb8fc9bd82040a5cb287922e9
maxx52 <gospelwork@yandex.ru>
2018-10-29 12:33:45 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+	<v-app>
+		<TopLine/>
+		<v-content>
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+		<v-footer app></v-footer>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import TopLine from '@/components/TopLine'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  },
-  data () {
+		TopLine
+	},
+  data() {
     return {
-      //
-    }
+			// rtr
+		}
   }
-}
+};
 </script>
---
+++ src/components/TopLine.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<v-navigation-drawer app temporary v-model="drawer"></v-navigation-drawer>
+
+		<v-toolbar app extended>
+			<v-toolbar-side-icon class="hidden-sm-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+
+			<v-spacer></v-spacer>
+			<v-toolbar-title>Муниципальное бюджетное дошкольное образовательное учреждение Детский сад №7 г. Павлово</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<div class="text-xs-center" slot="extension">
+				<v-menu open-on-hover bottom offset-y v-for="link of links" :key="link.id">
+					<v-btn slot="activator" flat class="caption">
+						<v-icon>{{link.icon}}</v-icon>
+						{{link.title}}
+						<v-icon>arrow_drop_down</v-icon>
+					</v-btn>
+
+					<v-list>
+						<v-list-tile v-for="link of links" :key="link.title" @click="">
+							<v-list-tile-title class="text-capitalized">{{ link.title }}</v-list-tile-title>
+						</v-list-tile>
+					</v-list>
+				</v-menu>
+			</div>
+		</v-toolbar>
+	</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      drawer: false,
+      links: [
+				{ title: "Главное меню", url: "/", id: "1" },
+				{ title: "Образовательные стандарты", url: "/", id: "2" },
+				{ title: "Дополнительно", url: "/", id: "3" },
+				{ title: "Для вас, родители", url: "/", id: "4" },
+				{ title: "Полезные ссылки", url: "/", id: "5" },
+				{ title: "Горячие линии", url: "/", id: "6" },
+				{ title: "", url: "/", id: "7", icon: 'remove_red_eye' }
+				]
+    };
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
